fe5cedb916962eade6aa94d8e82324ba1e4eff72
MagicNightFox <50232151+MagicNightFox@users.noreply.github.com>
2023-08-29 21:05:27 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function openclosed(){
+var currentdate = new Date();
+var currentday = currentdate.getDay();
+var currenthour = currentdate.getHours();
+//var currentminute = currentdate.getMinutes();
+
+if(currentday >=1 && currentday <=5 && currenthour >= 10 && currenthour <= 18){
+    document.getElementById("openclosed").style.color = "rgb(87, 155, 81)";
+return("OTEVŘENO");
+}
+else if(currentday == 6 && currenthour >= 9 && currenthour <=14){
+    document.getElementById("openclosed").style.color = "rgb(87, 155, 81)";
+    return("OTEVŘENO");
+}
+else{
+    document.getElementById("openclosed").style.color = "rgb(255, 73, 73)";
+    return("ZAVŘENO");
+}
+}
+
+function bigpic(img) {
+    viewwin = window.open(img.src,'viewwin', 'width=600,height=300');    
+}
+
+
+ function formOdeslan(){
+alert("Úspěšně odesláno");
+ }
